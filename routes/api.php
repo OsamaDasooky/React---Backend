2f82f365e972c114951b152d3913aa066a843f7c
Waad-Awajneh <waad.awajneh707@gmail.com>
2022-12-09 21:59:23 +0200

    articles api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Resources\ArticleResource;
+use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+//articles Using Resources
+Route::get('/articles', function () {
+    return ArticleResource::collection(Article::all());
 });
